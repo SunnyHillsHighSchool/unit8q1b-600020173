c02e4f9043a6e477d77b8d449074e951ad255ed6
Jonathan Hsing <>
2021-04-28 14:53:16 +0000

    added Unit8_1b test and debugged

--- Unit8_1b.java
+++ Unit8_1b.java
@@ -1,7 +1,17 @@
-public class Unit8_1a
+public class Unit8_1b
 {
   public int [] [] modifyRows (int [] [] array)
  { 
+   for(int a = 0; a < array.length; a++)
+   {
+     if(a%2 != 0)
+     {
+       for(int b = 0; b < array[a].length; b++)
+       {
+         array[a][b] = array[a - 1][b];
+       }
+     }
+   }
 return array;
       }
     }
